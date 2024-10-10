1065d1babdda94480645f328d5a8dfbcbeb9d360
Wagner Paulo <wagstalos@hotmail.com>
2024-10-10 09:57:05 -0300

    fix: image bg

--- js/scripts/imgBg.js
+++ js/scripts/imgBg.js
@@ -1,9 +1,21 @@
 document.addEventListener("DOMContentLoaded", function () {
   const lazyBgElements = document.querySelectorAll(".lazy-image-background");
 
+  const loadImage = (entry) => {
+    const bgImg = entry.target.getAttribute("data-bg");
+    entry.target.style.backgroundImage = `url(${bgImg})`;
+    observer.unobserve(entry.target); // Para evitar que a imagem seja carregada novamente
+  };
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        loadImage(entry);
+      }
+    });
+  });
+
   lazyBgElements.forEach((el) => {
-    const bgImg = el.getAttribute("data-bg");
-    el.style.backgroundImage = `url(${bgImg})`;
+    observer.observe(el);
   });
 });
-
